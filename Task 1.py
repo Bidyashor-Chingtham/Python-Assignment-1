a65ba379daad3ffe0fa878baa3546d5aa43c40eb
Bidyashor-Chingtham <bidyashorchingtham12345@gmail.com>
2025-03-05 18:00:15 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+# Task 1: Perform Basic Mathematical Operations
+a=int(input('Enter the first number: '))
+b=int(input('Enter the Second Number: '))
+c=a + b
+print('Addition' ,c)
+c=a - b
+print('Subtraction' ,c)
+c=a * b
+print('Multiplication: ',c)
+c=a / b
+print('Division: ',c)
---
+++ Task 2.py
@@ -0,0 +1,5 @@
+#Creating a Personalized Greeting
+
+a=input('Enter your First Name: ')
+b=input('Enter your Last Name: ')
+print('Hello,',a ,b,'! Welcome to the python Program.')
